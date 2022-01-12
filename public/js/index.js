59963b1fff2cbcade1b949b268c6c6360ddbe6df
clxxiii <earthnoartisjusteh@gmail.com>
2022-01-12 18:52:23 -0500

    links page

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,18 @@
-particlesJS.load("particles-bottom", "js/particles.json");
-particlesJS.load("particles-top", "js/particles.json");
+particlesJS.load("particles-bottom", "/js/particles.json");
+particlesJS.load("particles-top", "/js/particles.json");
 
-// Change position of card on mouse move
-window.addEventListener("mousemove", (event) => {
-	let xMid = window.innerWidth / 2;
-	let yMid = window.innerHeight / 2;
+// // Change position of card on mouse move
+// window.addEventListener("mousemove", (event) => {
+// 	let card = document.getElementById("card");
 
-	let rotationX = (event.clientX - xMid) / 20;
-	let rotationY = (event.clientY - yMid) / -20;
-	document.getElementById(
-		"card"
-	).style.transform = `rotateY(${rotationX}deg) rotateX(${rotationY}deg)`;
-	// console.log(document.getElementById("card"));
-});
+// 	let xMid = window.innerWidth / 2;
+// 	let yMid = window.innerHeight / 2;
+
+// 	let rotationX = (event.clientX - xMid) / 50;
+// 	let rotationY = (event.clientY - yMid) / -50;
+// 	card.style.transform = `rotateY(${rotationX}deg) rotateX(${rotationY}deg)`;
+// });
+
+function redirect(url) {
+	window.location.href = url;
+}
