1bd9aafc67b1d9600bcdf1b8ecaf63b7a058331d
clxxiii <earthnoartisjusteh@gmail.com>
2022-01-12 15:52:46 -0500

    wiggle wiggle wiggle

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,15 @@
-particlesJS.load("particles-js", "js/particles.json");
+particlesJS.load("particles-bottom", "js/particles.json");
+particlesJS.load("particles-top", "js/particles.json");
+
+// Change position of card on mouse move
+window.addEventListener("mousemove", (event) => {
+	let xMid = window.innerWidth / 2;
+	let yMid = window.innerHeight / 2;
+
+	let rotationX = (event.clientX - xMid) / 20;
+	let rotationY = (event.clientY - yMid) / -20;
+	document.getElementById(
+		"card"
+	).style.transform = `rotateY(${rotationX}deg) rotateX(${rotationY}deg)`;
+	// console.log(document.getElementById("card"));
+});
