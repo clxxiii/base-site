f47ea9d7e9bdf5e22ead0873d10bd87c6356b5dd
clxxiii <earthnoartisjusteh@gmail.com>
2022-01-29 23:46:44 -0500

    committing so grif can see it

--- public/js/projectBuilder.js
+++ public/js/projectBuilder.js
@@ -4,8 +4,33 @@ async function buildProjects() {
 	let projectData = await fetch("./projects.json");
 	projectData = await projectData.json();
 
-	const card = document.createElement("div");
-	card.classList.add("card");
-	card.innerHTML = projectData[0].name;
-	projects.appendChild(card);
+	projectData.forEach((proj) => {
+		// Create Elements
+		let card = document.createElement("a");
+		let name = document.createElement("div");
+		let desc = document.createElement("div");
+		let icon = document.createElement("img");
+		let stat = document.createElement("div");
+
+		card.classList.add("card");
+		name.classList.add("name");
+		desc.classList.add("desc");
+		icon.classList.add("icon");
+		stat.classList.add("stat");
+
+		icon.src = proj.icon_url;
+		card.href = proj.link;
+		name.innerHTML = proj.name;
+		desc.innerHTML = proj.desc;
+		stat.innerHTML = "<b>Status:</b> " + proj.status;
+
+		// Assemble Card
+		card.appendChild(icon);
+		card.appendChild(name);
+		card.appendChild(desc);
+		card.appendChild(stat);
+
+		// Append to list
+		projects.appendChild(card);
+	});
 }
