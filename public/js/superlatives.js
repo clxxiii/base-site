4b3f3062f8e1b4d1350e4ded4fb37fb671c8f801
clxxiii <earthnoartisjusteh@gmail.com>
2022-02-18 16:58:36 -0500

    Faster animation + I'm in your walls

--- public/js/superlatives.js
+++ public/js/superlatives.js
@@ -3,12 +3,13 @@ let superlatives = [
 	"a Rollercoaster Enthusiast",
 	"Addicted to Rainbow 6",
 	"Top 100k in osu!",
-	"Rubik's Cuber",
+	"a Rubik's Cuber",
 	"a Clone Hero Charter",
 	"S+ in TETRIO",
 	"an osu! Tourney Host",
 	"a Twitch Streamer",
 	"learning Front-end",
+	"in your walls",
 	"<b>clxxiii</b>",
 ];
 
@@ -22,11 +23,11 @@ function spinSuperWheel(input) {
 	let cube = document.getElementById("superlative-cube"),
 		faces = cube.getElementsByClassName("face");
 	faces[1].innerHTML = input;
-	cube.style.animation = "superlativespin 750ms ease-in-out 1";
+	cube.style.animation = "superlativespin 500ms ease-in-out 1";
 	setTimeout(() => {
 		faces[0].innerHTML = input;
 		setTimeout(() => {
 			cube.style.animation = "";
 		}, 100);
-	}, 730);
+	}, 480);
 }
