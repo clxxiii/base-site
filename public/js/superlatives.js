4fd97d8721aeb3667fababaa212c5c102f1a09cd
clxxiii <earthnoartisjusteh@gmail.com>
2022-02-18 15:24:35 -0500

    superlative main page

---
+++ public/js/superlatives.js
@@ -0,0 +1,32 @@
+let superlatives = [
+	"a CompSci Major",
+	"a Rollercoaster Enthusiast",
+	"Addicted to Rainbow 6",
+	"Top 100k in osu!",
+	"Rubik's Cuber",
+	"a Clone Hero Charter",
+	"S+ in TETRIO",
+	"an osu! Tourney Host",
+	"a Twitch Streamer",
+	"learning Front-end",
+	"<b>clxxiii</b>",
+];
+
+let currentIndex = 0;
+setInterval(() => {
+	spinSuperWheel(superlatives[currentIndex]);
+	currentIndex = (currentIndex + 1) % superlatives.length;
+}, 2000);
+
+function spinSuperWheel(input) {
+	let cube = document.getElementById("superlative-cube"),
+		faces = cube.getElementsByClassName("face");
+	faces[1].innerHTML = input;
+	cube.style.animation = "superlativespin 750ms ease-in-out 1";
+	setTimeout(() => {
+		faces[0].innerHTML = input;
+		setTimeout(() => {
+			cube.style.animation = "";
+		}, 100);
+	}, 730);
+}
