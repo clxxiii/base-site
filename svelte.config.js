ecdf81452a0c78e90946f2bbd09fdf4501e00dd8
clxxiii <clxxiii@users.noreply.github.com>
2026-02-22 00:32:54 -0500

    fully update dependcies

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
-    csrf: {
-      checkOrigin: false // THIS IS ONLY DISABLED FOR FILE UPLOADS. IF I EVER ADD SIGN IN/AUTHENTICATION TO THIS PROJECT, THIS NEEDS TO BE MOVED!
-    },
-    files: {
-      lib: 'src/lib'
-    }
-    
-  }
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
 
 export default config;
